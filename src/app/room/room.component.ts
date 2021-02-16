6950dd327ed55de137c898ac6842fb8c51e8c883
Salonie Samarwal <49318984+salonie222@users.noreply.github.com>
2021-02-16 17:52:59 +0530

    Add files via upload

---
+++ src/app/room/room.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+import { WebexService } from "../webex.service";
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './room.component.html',
+  styleUrls: ['./room.component.sass']
+})
+
+export class RoomComponent implements OnInit{
+  roomName: string;
+
+  constructor(private webex: WebexService) {}
+
+  ngOnInit(): void {
+    this.webex.onInit()
+  }
+
+  onCreateRoom() {
+    if(this.roomName) {
+      this.webex.onCreateRoom(this.roomName)
+    }
+  }
+
+  listRooms() {
+    this.webex.onListRoom().then((rooms) => {
+      console.log(rooms)
+    })
+  }
+
+  onLogout() {
+    this.webex.onLogout()
+  }
+  
+  addmember() {
+    if(this.roomName) {
+    this.webex.addmember(this.roomName)
+    }
+    }
+
+  sendmsg(){
+    if(this.roomName) {
+      this.webex.sendmsg(this.roomName)
+      }
+  }
+
+}
+
+
